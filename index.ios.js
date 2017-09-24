717657ccf90a331234c37a969f4f5497b2245ede
kellydern <kelly.dern@gmail.com>
2017-09-24 15:33:52 -0600

    setting up files

---
+++ index.ios.js
@@ -0,0 +1,9 @@
+// Index.ioS.js -- place code in here for iOS
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+// Render it to the device
